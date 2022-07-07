acb920f122fbe35129c5dc4c5a4e8f27d8c40ee5
niralmaruda <85137760+niralmaruda@users.noreply.github.com>
2022-07-07 19:33:45 +0530

    clear main file

--- Main.java
+++ Main.java
@@ -8,17 +8,5 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        // example 1: adding customer.
-       var service = new CustomerService();
-       var command = new AddCustomerCommand(service);
-       var button = new Button(command);
-       button.click();
-
-
-       // example 2: execute list of command.
-       var composite = new CompositeCommand();
-       composite.add(new ResizeCommand());
-       composite.add(new BlackAndWhiteCommand());
-       composite.execute();
     }
 }
